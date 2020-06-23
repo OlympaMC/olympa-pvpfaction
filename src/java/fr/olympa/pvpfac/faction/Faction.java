664fbf70eeadca3e1006754c6e2c71c82f8cbe29
Tristiisch <tristiisch74@hotmail.com>
2020-06-23 05:03:41 +0200

    up

--- src/java/fr/olympa/pvpfac/faction/Faction.java
+++ src/java/fr/olympa/pvpfac/faction/Faction.java
@@ -27,8 +27,8 @@ import fr.olympa.pvpfac.PvPFaction;
 import fr.olympa.pvpfac.player.FactionPlayer;
 
 public class Faction extends Clan<Faction> {
-	
-	private static FixedLine<Scoreboard<FactionPlayer>> header = new FixedLine<>("§e§oMa Faction:");
+
+	private static FixedLine<Scoreboard<FactionPlayer>> header = new FixedLine<>("§7§oMa Faction:");
 	private static TimerLine<Scoreboard<FactionPlayer>> players = new TimerLine<>((x) -> {
 		FactionPlayer fp = x.getOlympaPlayer();
 		Faction faction = fp.getClan();
@@ -48,16 +48,16 @@ public class Faction extends Clan<Faction> {
 		return joiner.toString();
 	}, PvPFaction.getInstance(), 10);
 	//	List<String> oldName = new ArrayList<>();
-
+	
 	Set<FactionClaim> claims = new HashSet<>();
 	String tag;
 	String description;
 	Location home;
-	
+
 	public Faction(ClansManager<Faction> manager, int id, String name, long chief, int maxSize) {
 		super(manager, id, name, chief, maxSize);
 	}
-	
+
 	public Faction(ClansManager<Faction> manager, int id, String name, long chief, int maxSize, double money, long created, String tag, String description, Location home, Set<FactionClaim> claims) {
 		super(manager, id, name, chief, maxSize, money, created);
 		this.tag = tag;
@@ -65,53 +65,53 @@ public class Faction extends Clan<Faction> {
 		this.home = home;
 		this.claims = claims;
 	}
-	
+
 	public void claim(Chunk chunk) {
 		claims.add(new FactionClaim(chunk));
 	}
-
+	
 	public Set<FactionPlayer> getOnlinePlayers() {
 		return getPlayers().stream().map(p -> (FactionPlayer) AccountProvider.get(p.getUniqueId())).collect(Collectors.toSet());
 	}
-	
+
 	public OlympaFactionRole getRole(Player player) {
 		return null;
 	}
-
+	
 	public int getPower() {
 		return getPlayers().stream().mapToInt(p -> ((FactionPlayer) AccountProvider.get(p.getUniqueId())).getPower()).sum();
 	}
-	
+
 	public boolean hasClaim(Chunk chunk) {
 		return claims.stream().anyMatch(claim -> claim.isChunk(chunk));
 	}
-	
+
 	public boolean isOverClaimable() {
 		return false;
 	}
-	
+
 	@Override
 	public void memberJoin(ClanPlayerInterface<Faction> member) {
 		super.memberJoin(member);
-		
+
 		Scoreboard<FactionPlayer> scoreboard = PvPFaction.getInstance().scoreboards.getPlayerScoreboard((FactionPlayer) member);
 		scoreboard.addLine(FixedLine.EMPTY_LINE);
 		scoreboard.addLine(header);
 		scoreboard.addLine(players);
 	}
-	
+
 	@Override
 	protected void removedOnlinePlayer(ClanPlayerInterface<Faction> oplayer) {
 		super.removedOnlinePlayer(oplayer);
-
+		
 		PvPFaction.getInstance().scoreboards.removePlayerScoreboard((FactionPlayer) oplayer);
 		PvPFaction.getInstance().scoreboards.create((FactionPlayer) oplayer);
 	}
-	
+
 	public boolean unclaim(Chunk chunk) {
 		return claims.removeIf(claim -> claim.isChunk(chunk));
 	}
-	
+
 	private void updateClaims() {
 		try {
 			PreparedStatement statement = manager.updateClanMoneyStatement.getStatement();
@@ -123,7 +123,7 @@ public class Faction extends Clan<Faction> {
 			broadcast("Une erreur est survenue.");
 		}
 	}
-
+	
 	public Set<FactionPlayer> getOnlinePlayers(OlympaFactionRole officer) {
 		return null;
 	}
