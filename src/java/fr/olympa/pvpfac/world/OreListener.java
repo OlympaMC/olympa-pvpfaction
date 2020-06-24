7cc8e75fa317a2d54ea3e177dd9d8b92748fc176
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 20:10:38 +0200

    up

--- src/java/fr/olympa/pvpfac/world/OreListener.java
+++ src/java/fr/olympa/pvpfac/world/OreListener.java
@@ -7,14 +7,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkPopulateEvent;
 
+import fr.olympa.api.utils.spigot.SpigotUtils;
 import fr.olympa.pvpfac.PvPFaction;
 
 public class OreListener implements Listener {
-
+	
 	@EventHandler
 	public void onChunkPopulate(ChunkPopulateEvent event) {
-		//		if (!event.isNewChunk())
-		//			return;
 		Chunk chunk = event.getChunk();
 		PvPFaction.getInstance().getTask().runTaskAsynchronously(() -> {
 			int i = 0;
@@ -22,13 +21,14 @@ public class OreListener implements Listener {
 				for (int iX = 0; 16 > iX; iX++)
 					for (int iZ = 0; 16 > iZ; iZ++) {
 						Block block = chunk.getBlock(iX, iY, iZ);
+						
 						if (block.getType().name().contains("_ORE")) {
 							i++;
-							//							PvPFaction.getInstance().getTask().runTask(() -> block.setType(Material.COBWEB));
-							block.setType(Material.COBWEB);
+							PvPFaction.getInstance().getTask().runTask(() -> block.setType(Material.COBWEB));
+							//							block.setType(Material.COBWEB);
 						}
 					}
-			System.out.println("ORE " + i + " " + chunk.getX() + " " + chunk.getZ());
+			System.out.println("ORE " + i + " " + chunk.getX() + " " + chunk.getZ() + SpigotUtils.convertLocationToString(chunk.getBlock(0, 0, 0).getLocation()));
 		});
 	}
 }
