34582f7a4429491ca3bec948936130948ff39f14
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 19:17:42 +0200

    up

--- src/java/fr/olympa/pvpfac/world/OreListener.java
+++ src/java/fr/olympa/pvpfac/world/OreListener.java
@@ -1,8 +1,5 @@
 package fr.olympa.pvpfac.world;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -11,21 +8,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 
 public class OreListener implements Listener {
-
-	private static List<Material> ores = Arrays.asList(Material.DIAMOND_ORE, Material.GOLD_ORE, Material.IRON_ORE, Material.EMERALD_ORE, Material.LAPIS_ORE, Material.COAL_ORE);
 	
 	@EventHandler
 	public void onChunkLoad(ChunkLoadEvent event) {
 		if (!event.isNewChunk())
 			return;
 		Chunk chunk = event.getChunk();
-		for (int iY = 0; 256 > iY; iY++)
+		for (int iY = 0; 132 > iY; iY++)
 			for (int iX = 0; 16 > iX; iX++)
 				for (int iZ = 0; 16 > iZ; iZ++) {
 					Block block = chunk.getBlock(iY, iX, iZ);
-					if (block.getType() == Material.STONE)
+					if (block.getType() == Material.GRASS)
 						block.setType(Material.AIR);
-					else if (ores.contains(block.getType()))
+					else if (block.getType().name().contains("_ORE"))
 						block.setType(Material.COBWEB);
 				}
 	}
