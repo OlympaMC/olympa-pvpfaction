cac1582bc98094455d00f61f2844e78530e8e8fe
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 08:54:40 +0200

    ajouts de title pour les entrées dans un claim

--- src/java/fr/olympa/pvpfac/PvPFaction.java
+++ src/java/fr/olympa/pvpfac/PvPFaction.java
@@ -14,45 +14,47 @@ import fr.olympa.api.scoreboard.sign.ScoreboardManager;
 import fr.olympa.core.spigot.OlympaCore;
 import fr.olympa.pvpfac.faction.FactionManager;
 import fr.olympa.pvpfac.faction.chat.FactionChatListener;
+import fr.olympa.pvpfac.faction.claim.FactionClaimListener;
 import fr.olympa.pvpfac.faction.claim.FactionPvPListener;
 import fr.olympa.pvpfac.player.FactionPlayer;
 
 public class PvPFaction extends OlympaAPIPlugin {
-
+	
 	private static PvPFaction instance;
-
+	
 	public static PvPFaction getInstance() {
 		return instance;
 	}
-
+	
 	public ScoreboardManager<FactionPlayer> scoreboards;
 	public FactionManager factionManager;
-
+	
 	public FactionManager getFactionManager() {
 		return factionManager;
 	}
-	
+
 	public DynamicLine<Scoreboard<FactionPlayer>> lineMoney = new DynamicLine<>(x -> "§7Monnaie: §6" + x.getOlympaPlayer().getGameMoney().getFormatted());
 	public DynamicLine<Scoreboard<FactionPlayer>> lineGroup = new DynamicLine<>(x -> "§7Rang: §b" + x.getOlympaPlayer().getGroupNameColored());
-
+	
 	@Override
 	public void onDisable() {
 		scoreboards.unload();
 		sendMessage("§4" + getDescription().getName() + "§c (" + getDescription().getVersion() + ") est désactiver.");
 	}
-
+	
 	@Override
 	public void onEnable() {
 		instance = this;
 		super.onEnable();
-
+		
 		OlympaPermission.registerPermissions(PvPFactionPermission.class);
 		AccountProvider.setPlayerProvider(FactionPlayer.class, FactionPlayer::new, "pvpfac", FactionPlayer.COLUMNS);
 		//new FactionCommand(this).register();
-
+		
 		PluginManager pluginManager = getServer().getPluginManager();
 		pluginManager.registerEvents(new FactionChatListener(), this);
 		pluginManager.registerEvents(new FactionPvPListener(), this);
+		pluginManager.registerEvents(new FactionClaimListener(), this);
 		try {
 			pluginManager.registerEvents(factionManager = new FactionManager(), this);
 		} catch (Exception ex) {
@@ -62,7 +64,7 @@ public class PvPFaction extends OlympaAPIPlugin {
 		// pluginManager.registerEvents(new FactionJoinListener(), this);
 		// .registerEvents(new FactionChatListener(), this);
 		// pluginManager.registerEvents(new FactionPvPListener(), this);
-
+		
 		scoreboards = new ScoreboardManager(this, "§6Olympa §e§lPvPFaction").addLines(
 				FixedLine.EMPTY_LINE,
 				lineMoney,
@@ -70,7 +72,7 @@ public class PvPFaction extends OlympaAPIPlugin {
 				lineGroup,
 				FixedLine.EMPTY_LINE,
 				AnimLine.olympaAnimation());
-
+		
 		ProtocolAction protocolSupport = OlympaCore.getInstance().getProtocolSupport();
 		if (protocolSupport != null)
 			protocolSupport.disable1_8();
---
+++ src/java/fr/olympa/pvpfac/faction/claim/FactionClaimListener.java
@@ -0,0 +1,33 @@
+package fr.olympa.pvpfac.faction.claim;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import fr.olympa.api.utils.spigot.SpigotUtils;
+import fr.olympa.pvpfac.PvPFaction;
+import fr.olympa.pvpfac.faction.Faction;
+import fr.olympa.pvpfac.faction.FactionManager;
+
+public class FactionClaimListener implements Listener {
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerMove(PlayerMoveEvent event) {
+		Location from = event.getFrom();
+		Location to = event.getTo();
+		if (SpigotUtils.isSameLocation(from, to))
+			return;
+		Player player = event.getPlayer();
+		FactionManager manager = PvPFaction.getInstance().getFactionManager();
+		Faction fChunkTo = manager.getByChunk(to.getChunk());
+		if (fChunkTo != null) {
+			Faction fChunkFrom = manager.getByChunk(from.getChunk());
+			if (fChunkFrom.getID() == fChunkTo.getID())
+				return;
+			player.sendTitle(fChunkTo.getName(), fChunkTo.getDescription(), 0, 20, 20);
+		}
+	}
+}
