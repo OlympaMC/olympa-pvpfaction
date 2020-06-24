f23e1fe48c4939b5b73e4c07a0f1c6e4bcf77f88
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 08:30:51 +0200

    up

--- src/java/fr/olympa/pvpfac/faction/FactionManager.java
+++ src/java/fr/olympa/pvpfac/faction/FactionManager.java
@@ -14,27 +14,34 @@ import com.google.gson.reflect.TypeToken;
 import fr.olympa.api.clans.ClanPlayerInterface;
 import fr.olympa.api.clans.ClansManager;
 import fr.olympa.api.provider.AccountProvider;
+import fr.olympa.api.sql.OlympaStatement;
+import fr.olympa.api.sql.OlympaStatement.StatementType;
 import fr.olympa.api.utils.spigot.SpigotUtils;
 import fr.olympa.pvpfac.PvPFaction;
 import fr.olympa.pvpfac.PvPFactionPermission;
 import fr.olympa.pvpfac.faction.claim.FactionClaim;
 
 public class FactionManager extends ClansManager<Faction> {
+
+	public OlympaStatement updateFactionClaimsStatement;
+	public OlympaStatement updateFactionHomeStatement;
 	
 	public FactionManager() throws SQLException, ReflectiveOperationException {
 		super(PvPFaction.getInstance(), "pvpfac_faction", 10);
 		new FactionCommand<>(this, "faction", "Permet de gérer les factions.", PvPFactionPermission.FACTION_PLAYERS_COMMAND, "factions", "f", "fac").register();
+		updateFactionClaimsStatement = new OlympaStatement(StatementType.UPDATE, tableName, "id", "claims");
+		updateFactionHomeStatement = new OlympaStatement(StatementType.UPDATE, tableName, "id", "home");
 	}
-	
+
 	@Override
 	protected Faction createClan(int id, String name, long chief, int maxSize) {
 		return new Faction(this, id, name, chief, maxSize);
 	}
-	
+
 	public Faction getByChunk(Chunk chunk) {
 		return getClans().stream().filter(c -> c.getValue().hasClaim(chunk)).map(e -> e.getValue()).findFirst().orElse(null);
 	}
-	
+
 	@Override
 	public StringJoiner addDBCollums(StringJoiner columnsJoiner) {
 		columnsJoiner = super.addDBCollums(columnsJoiner);
@@ -47,7 +54,7 @@ public class FactionManager extends ClansManager<Faction> {
 		columnsJoiner.add("`claims` TEXT(65535) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci'");
 		return columnsJoiner;
 	}
-	
+
 	@Override
 	protected Faction provideClan(int id, String name, long chief, int maxSize, double money, long created, ResultSet resultSet) throws SQLException {
 		String jsonClaims = resultSet.getString("claims");
@@ -57,15 +64,15 @@ public class FactionManager extends ClansManager<Faction> {
 			}.getType());
 		return new Faction(this, id, name, chief, maxSize, money, created, resultSet.getString("tag"), resultSet.getString("description"), SpigotUtils.convertStringToLocation(resultSet.getString("home")), claims);
 	}
-
+	
 	public Faction getByName(String name) {
 		return getClans().stream().filter(c -> c.getValue().getName().equalsIgnoreCase(name)).map(c -> c.getValue()).findFirst().orElse(null);
 	}
-	
+
 	public Faction getByTag(String tag) {
 		return getClans().stream().filter(c -> c.getValue().getTag().equalsIgnoreCase(tag)).map(c -> c.getValue()).findFirst().orElse(null);
 	}
-	
+
 	public Faction get(String nameOrTagOrPlayer) throws SQLException {
 		Faction faction = getByName(nameOrTagOrPlayer);
 		if (faction == null)
@@ -76,5 +83,5 @@ public class FactionManager extends ClansManager<Faction> {
 		}
 		return faction;
 	}
-
+	
 }
