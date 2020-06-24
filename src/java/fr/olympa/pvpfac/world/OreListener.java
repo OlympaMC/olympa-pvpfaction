fbb61de9eddff7d6c75e98d6825c649d70992236
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 19:50:42 +0200

    up

--- src/java/fr/olympa/pvpfac/world/OreListener.java
+++ src/java/fr/olympa/pvpfac/world/OreListener.java
@@ -7,28 +7,27 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 
+import fr.olympa.pvpfac.PvPFaction;
+
 public class OreListener implements Listener {
 	
 	@EventHandler
 	public void onChunkLoad(ChunkLoadEvent event) {
-		//		if (!event.isNewChunk())
-		//			return;
+		if (!event.isNewChunk())
+			return;
 		Chunk chunk = event.getChunk();
-		new Thread() {
-			@Override
-			public void run() {
-				int i = 0;
-				for (int iY = 0; 132 > iY; iY++)
-					for (int iX = 0; 16 > iX; iX++)
-						for (int iZ = 0; 16 > iZ; iZ++) {
-							Block block = chunk.getBlock(iX, iY, iZ);
-							if (block.getType().name().contains("_ORE")) {
-								i++;
-								block.setType(Material.COBWEB);
-							}
+		PvPFaction.getInstance().getTask().runTaskAsynchronously(() -> {
+			int i = 0;
+			for (int iY = 0; 132 > iY; iY++)
+				for (int iX = 0; 16 > iX; iX++)
+					for (int iZ = 0; 16 > iZ; iZ++) {
+						Block block = chunk.getBlock(iX, iY, iZ);
+						if (block.getType().name().contains("_ORE")) {
+							i++;
+							PvPFaction.getInstance().getTask().runTask(() -> block.setType(Material.COBWEB));
 						}
-				System.out.println("ORE " + i + " " + chunk.getX() + " " + chunk.getZ());
-			}
-		}.start();
+					}
+			System.out.println("ORE " + i + " " + chunk.getX() + " " + chunk.getZ());
+		});
 	}
 }
